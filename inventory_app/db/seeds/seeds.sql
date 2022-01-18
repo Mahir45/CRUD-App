466580744a583e7a20ba0c8c0f9e2a03518ad872
Mahir Ahmed <mahir.ahmed56@hotmail.com>
2022-01-17 21:09:30 -0500

    edit and index page bug fix

--- inventory_app/db/seeds/seeds.sql
+++ inventory_app/db/seeds/seeds.sql
@@ -1,9 +1,7 @@
+INSERT INTO warehouse (name, city) VALUES ('warehouse1', 'Mississauga'), ('warehouse2', 'Brampton' ), ('warehouse3', 'New York');
+
 INSERT INTO inventory (name, description, total, location, warehouse_id) VALUES('flashlight', 'sfjbkfsbkbf', 7, 'Mississauga', '1'),
-('YOu','dfjnbdfnbdb',6,'Brampton','1'),
-('BET','sdlfnvldfn', 8,'Ontario','1');
-
-
-INSERT INTO warehouse (name, city) VALUES ('warehouse1', 'Mississauga'), ('warehouse2', 'Brampton' );
-
+('YOu','dfjnbdfnbdb',6,'Brampton','2'),
+('BET','sdlfnvldfn', 8,'Ontario','3');
 
 
--- inventory_app/routes/index.js
+++ inventory_app/routes/index.js
@@ -1,26 +1,36 @@
 const express = require("express");
 const router = express.Router();
 
-
 /* GET home page. */
 module.exports = (db) => {
-router.get("/", (req, res) => {
-  db.query(`SELECT * FROM inventory;`)
+  router.get("/", (req, res) => {
+    db.query(
+      `
+  SELECT 
+	inventory.id,
+  warehouse.name,
+	inventory.total,
+	inventory.description,
+  inventory.location
+FROM
+	inventory
+ LEFT JOIN warehouse
+    ON warehouse.id = inventory.warehouse_id
 
-    .then((result) => {
-     
-      const templateVars = {
-        rows: result.rows
-      }
-  res.render("index", templateVars);
-    })
-    .catch((err) => {
-      console.log(err + "error");
-    });
-});
+  ;`
+    )
 
+      .then((result) => {
+        console.log(result.rows)
+        const templateVars = {
+          rows: result.rows,
+        };
+        res.render("index", templateVars);
+      })
+      .catch((err) => {
+        console.log(err + "error");
+      });
+  });
 
-return router;
-}
-
-
+  return router;
+};
